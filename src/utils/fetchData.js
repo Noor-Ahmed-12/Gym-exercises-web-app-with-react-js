7a4811201de1a3affc5eb6c72eb9168a69b09628
Noor Ahmed Shaikh <60597502+Noor-Ahmed-12@users.noreply.github.com>
2022-07-25 17:44:19 -0700

    Update fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -9,7 +9,7 @@ export const exerciseOptions =  {
 export const youtubeOptions =  {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '06fabe01bbmsh190500788201173p15bdcejsn4b87b08dbe22',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
@@ -17,7 +17,7 @@ export const youtubeOptions =  {
 export const BMIOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '06fabe01bbmsh190500788201173p15bdcejsn4b87b08dbe22',
+		'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
 		'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com'
 	}
 };
